b59023ae339feec7337a57ce408ff709afdd2b84
Andrew Wu <chicken@chicken-house.net>
2020-10-06 01:14:50 +0800

    init repo...

---
+++ GameHost1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace GameHost1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] matrix = new bool[50, 50];
+
+            Init(matrix);
+
+            for (int count = 0; count < 5000; count++)
+            {
+                Thread.Sleep(100);
+                TimePass(matrix);
+
+                Console.Clear();
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    for (int y = 0; y < matrix.GetLength(0); y++)
+                    {
+                        bool value = matrix[x, y];
+                        Console.Write(value ? '★' : '☆');
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+
+        static void Init(bool[,] matrix)
+        {
+            matrix[1, 1] = true;
+            matrix[3, 25] = true;
+        }
+
+        static void TimePass(bool[,] matrix)
+        {
+            // TODO: fill your code here
+        }
+    }
+}
