3290c49c7c15f48e652cfc23294d0e538b591e84
Andrew Wu <chicken@chicken-house.net>
2020-10-08 21:45:39 +0800

    簡化 TimePass 判斷邏輯。 GameHost 只會傳遞指定位置周圍的狀態

--- GameHost1/Program.cs
+++ GameHost1/Program.cs
@@ -8,37 +8,72 @@ namespace GameHost1
         static void Main(string[] args)
         {
             bool[,] matrix = new bool[50, 50];
+            bool[,] area = new bool[3, 3];
 
             Init(matrix);
 
             for (int count = 0; count < 5000; count++)
             {
-                Thread.Sleep(100);
-                TimePass(matrix);
+                int live_count = 0;
+                Thread.Sleep(200);
 
-                Console.Clear();
-                for (int x = 0; x < matrix.GetLength(1); x++)
+                Console.SetCursorPosition(0, 0);
+                for (int y = 0; y < matrix.GetLength(0); y++)
                 {
-                    for (int y = 0; y < matrix.GetLength(0); y++)
+                    for (int x = 0; x < matrix.GetLength(1); x++)
                     {
-                        bool value = matrix[x, y];
-                        Console.Write(value ? '★' : '☆');
+                        // clone area
+                        for (int ay = 0; ay < 3; ay++)
+                        {
+                            for (int ax = 0; ax < 3; ax++)
+                            {
+                                int cx = x - 1 + ax;
+                                int cy = y - 1 + ay;
+
+                                if (cx < 0) area[ax, ay] = false;
+                                else if (cy < 0) area[ax, ay] = false;
+                                else if (cx >= matrix.GetLength(1)) area[ax, ay] = false;
+                                else if (cy >= matrix.GetLength(0)) area[ax, ay] = false;
+                                else area[ax, ay] = matrix[cx, cy];
+                            }
+                        }
+                        matrix[x, y] = TimePassRule(area);
+                        Console.Write(matrix[x, y]? '★' : '☆');
+                        if (matrix[x, y]) live_count++;
                     }
                     Console.WriteLine();
                 }
+                Console.WriteLine($"total lives: {live_count}, round: {count} / 5000...");
             }
         }
 
 
         static void Init(bool[,] matrix)
         {
-            matrix[1, 1] = true;
-            matrix[3, 25] = true;
+            Random rnd = new Random();
+            int rate = 20;
+
+            for (int y = 0; y < matrix.GetLength(0); y++)
+            {
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    matrix[x, y] = (rnd.Next(100) < rate);
+                }
+            }
         }
 
-        static void TimePass(bool[,] matrix)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="area">must be bool[3, 3]</param>
+        /// <returns></returns>
+        static bool TimePassRule(bool[,] area)
         {
             // TODO: fill your code here
+
+
+            return false;
         }
     }
 }
