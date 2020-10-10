5bc69472d6e8ac99d3346231908b5ea8de10787b
Andrew Wu <chicken@chicken-house.net>
2020-10-11 00:16:03 +0800

    add: class diagram note: reformat codes fix: incorrect use of array dimation

--- GameHost1/Program.cs
+++ GameHost1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace GameHost1
@@ -8,20 +9,28 @@ namespace GameHost1
     {
         static void Main(string[] args)
         {
-            bool[,] matrix = new bool[50, 50];
-            bool[,] area = new bool[3, 3];
+            // configure
+            int total_cycle_count = 5000;
+            int map_fill_rate = 80;
+            (int x, int y) map_size = (100, 50);
+            
+            
+            // init
+            bool[,] matrix = new bool[map_size.x, map_size.y];
+            Init(matrix, map_fill_rate);
+            Console.Clear();
 
-            Init(matrix, 10);
 
-            for (int count = 0; count < 5000; count++)
+            // run
+            for (int count = 0; count < total_cycle_count; count++)
             {
                 int live_count = 0;
                 Thread.Sleep(200);
 
                 Console.SetCursorPosition(0, 0);
-                for (int y = 0; y < matrix.GetLength(0); y++)
+                for (int y = 0; y < matrix.GetLength(1); y++)
                 {
-                    for (int x = 0; x < matrix.GetLength(1); x++)
+                    for (int x = 0; x < matrix.GetLength(0); x++)
                     {
                         var cell = new Life(matrix[x, y], (x, y));
                         var mapcx = new MapContext(matrix, cell);
@@ -32,18 +41,19 @@ namespace GameHost1
                     }
                     Console.WriteLine();
                 }
-                Console.WriteLine($"total lives: {live_count}, round: {count} / 5000...");
+                Console.WriteLine($"world size: ({map_size.x}, {map_size.y}), life fill rate: {map_fill_rate} %");
+                Console.WriteLine($"total lives: {live_count}, round: {count} / {total_cycle_count}...");
             }
         }
 
 
-        static void Init(bool[,] matrix, int rate = 20)
+        static void Init(bool[,] matrix, int rate)
         {
             Random rnd = new Random();
 
-            for (int y = 0; y < matrix.GetLength(0); y++)
+            for (int y = 0; y < matrix.GetLength(1); y++)
             {
-                for (int x = 0; x < matrix.GetLength(1); x++)
+                for (int x = 0; x < matrix.GetLength(0); x++)
                 {
                     matrix[x, y] = (rnd.Next(100) < rate);
                 }
@@ -52,11 +62,10 @@ namespace GameHost1
     }
 
 
-    internal class Life
+    public class Life
     {
         public bool IsLive { get; private set; }
-        public (int x, int y) Position { get; private set; }
-        
+        public (int x, int y) Position { get; private set; }        
 
         public Life(bool status, (int x, int y) position)
         {
@@ -72,19 +81,13 @@ namespace GameHost1
 
         public bool NextCycle(MapContext mcx)
         {
-            int live_count = 0;
+            int live_count = mcx.SearchLivesAroundMe().Sum(x => (x.IsLive)?(1):(0));
 
-            foreach(var n in mcx.SearchLivesAroundMe())
-            {
-                if (n == null) continue;
-                if (n.IsLive) live_count++;
-            }
-
-            return _gameRules[this.IsLive][live_count];
+            return this.IsLive = _gameRules[this.IsLive][live_count];
         }
     }
 
-    internal class MapContext
+    public class MapContext
     {
         private bool[,] _world;
         private Life _target;
