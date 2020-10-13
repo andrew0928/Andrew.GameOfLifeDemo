b0b2712a253cf7ec54443545bb7bd31eeffc30a8
Stuart Hsu <stuarthsu@91app.com>
2020-10-13 23:08:23 +0800

    milestone1

--- GameHost1/Program.cs
+++ GameHost1/Program.cs
@@ -53,10 +53,10 @@ namespace GameHost1
                 matrix_current = matrix_next;
                 matrix_next = matrix_temp;
 
-#if (DEBUG)
-                // debug: clean up [next] map..
-                for (int y = 0; y < matrix_next.GetLength(1); y++) for (int x = 0; x < matrix_next.GetLength(0); x++) matrix_next[x, y] = false;
-#endif
+                #if (DEBUG)
+                    // debug: clean up [next] map..
+                    for (int y = 0; y < matrix_next.GetLength(1); y++) for (int x = 0; x < matrix_next.GetLength(0); x++) matrix_next[x, y] = false;
+                #endif
 
                 Console.WriteLine($"total lives: {live_count}, round: {count} / 5000...");
             }
@@ -90,17 +90,63 @@ namespace GameHost1
         }
 
 
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="area">must be bool[3, 3]</param>
-            /// <returns></returns>
-            static bool TimePassRule(bool[,] area)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="area">must be bool[3, 3]</param>
+        /// <returns></returns>
+        static bool TimePassRule(bool[,] area)
         {
             // TODO: fill your code here
+            int live_cell = 0;
 
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (area[x, y] == true)
+                    {
+                        live_cell++;
+                    }
+                }
+            }
 
-            return area[1, 1];
+            if (area[1, 1] == true)
+            {
+                live_cell--;
+
+                if (live_cell < 2)
+                {
+                    return false;
+                }
+                else if (live_cell == 2 || live_cell == 3)
+                {
+                    return true;
+                }
+                else // (live_cell > 3)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (live_cell == 3)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
 }
+
+// 生命遊戲中，對於任意細胞，規則如下：
+// 每個細胞有兩種狀態 - 存活或死亡，每個細胞與以自身為中心的周圍八格細胞產生互動（如圖，黑色為存活，白色為死亡）
+// 當前細胞為存活狀態時，當周圍的存活細胞低於2個時（不包含2個），該細胞變成死亡狀態。（模擬生命數量稀少）
+// 當前細胞為存活狀態時，當周圍有2個或3個存活細胞時，該細胞保持原樣。
+// 當前細胞為存活狀態時，當周圍有超過3個存活細胞時，該細胞變成死亡狀態。（模擬生命數量過多）
+// 當前細胞為死亡狀態時，當周圍有3個存活細胞時，該細胞變成存活狀態。（模擬繁殖）
+// 可以把最初的細胞結構定義為種子，當所有在種子中的細胞同時被以上規則處理後，可以得到第一代細胞圖。按規則繼續處理當前的細胞圖，可以得到下一代的細胞圖，周而復始。
