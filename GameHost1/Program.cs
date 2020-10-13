1bf3eb3a0ba431ad68db6f61fae20d3bd212b219
Andrew Wu <andrewwu@nine-yi.com>
2020-10-13 13:12:33 +0800

    add unit test - rules test - patterns test

--- GameHost1/Program.cs
+++ GameHost1/Program.cs
@@ -9,168 +9,14 @@ namespace GameHost1
 
 
 
-
-
-
-
-
         static void Main(string[] args)
         {
-            #region run test case
-            RunUnitTest(
-                "穩定狀態: 板凳(1)", 5,
-                new List<string[]>()
-                {
-                    new string[]
-                    {
-                        "11",
-                        "11",
-                    }
-                });
-
-            RunUnitTest(
-                "穩定狀態: 麵包(1)", 5,
-                new List<string[]>()
-                {
-                    new string[]
-                    {
-                        "000000",
-                        "001100",
-                        "010010",
-                        "001010",
-                        "000100",
-                        "000000",
-                    }
-                });
-
-            RunUnitTest(
-                "穩定狀態: 蜂巢(1)", 5,
-                new List<string[]>()
-                {
-                    new string[]
-                    {
-                        "000000",
-                        "001100",
-                        "010010",
-                        "001100",
-                        "000000",
-                    }
-                });
-
-
-            RunUnitTest(
-                "震盪狀態: 信號燈(2)", 5,
-                new List<string[]>()
-                {
-                    new string[] {
-                        "00000",
-                        "00000",
-                        "01110",
-                        "00000",
-                        "00000"
-                    },
-                    new string[] {
-                        "00000",
-                        "00100",
-                        "00100",
-                        "00100",
-                        "00000"
-                    },
-                });
-
-            // 震盪狀態: 蟾蜍(2)
-            RunUnitTest(
-                "震盪狀態: 蟾蜍(2)", 5,
-                new List<string[]>()
-                {
-                    new string[] {
-                        "000000",
-                        "000000",
-                        "001110",
-                        "011100",
-                        "000000",
-                        "000000",
-                    },
-                    new string[] {
-                        "000000",
-                        "000100",
-                        "010010",
-                        "010010",
-                        "001000",
-                        "000000",
-                    },
-                });
-
-            RunUnitTest(
-                "震盪狀態: 烽火 (2)", 5,
-                new List<string[]>()
-                {
-                    new string[] {
-                        "000000",
-                        "011000",
-                        "010000",
-                        "000010",
-                        "000110",
-                        "000000",
-                    },
-                    new string[] {
-                        "000000",
-                        "011000",
-                        "011000",
-                        "000110",
-                        "000110",
-                        "000000",
-                    },
-                });
-
-            RunUnitTest(
-                "會移動的振盪狀態: 滑翔機 (4)", 1,
-                new List<string[]>()
-                {
-                    new string[] {
-                        "000000",
-                        "010000",
-                        "001100",
-                        "011000",
-                        "000000",
-                        "000000",
-                    },
-                    new string[] {
-                        "000000",
-                        "001000",
-                        "000100",
-                        "011100",
-                        "000000",
-                        "000000",
-                    },
-                    new string[] {
-                        "000000",
-                        "000000",
-                        "010100",
-                        "001100",
-                        "001000",
-                        "000000",
-                    },
-                    new string[] {
-                        "000000",
-                        "000000",
-                        "000100",
-                        "010100",
-                        "001100",
-                        "000000",
-                    },
-                });
-
-            Console.WriteLine("PRESS [ENTER] to continue...");
-            Console.ReadLine();
-            #endregion
-
             RunGameOfLife();
         }
 
 
         private static void RunGameOfLife()
-        { 
+        {
             bool[,] matrix = new bool[50, 20];
 
             Init(matrix, 20);
@@ -181,7 +27,7 @@ namespace GameHost1
                 Thread.Sleep(200);
                 Console.SetCursorPosition(0, 0);
 
-                for(int y = 0; y < matrix.GetLength(1); y++)
+                for (int y = 0; y < matrix.GetLength(1); y++)
                 {
                     for (int x = 0; x < matrix.GetLength(0); x++)
                     {
@@ -198,7 +44,7 @@ namespace GameHost1
         }
 
 
-        private static bool[,] GetNextGenMatrix(bool[,] matrix_current)
+        public static bool[,] GetNextGenMatrix(bool[,] matrix_current)
         {
             bool[,] matrix_next = new bool[matrix_current.GetLength(0), matrix_current.GetLength(1)];
             bool[,] area = new bool[3, 3];
@@ -262,84 +108,14 @@ namespace GameHost1
             {
                 for (int ax = 0; ax < area.GetLength(0); ax++)
                 {
-                    Console.Write("["+ax+",");
-                    Console.Write(ay+"]");
                     if (ax == center_x && ay == center_y) continue;
                     if (area[ax, ay] == true) lives++;
                 }
             }
-
             if (center == true && lives >= 2 && lives <= 3) return true;
             if (center == false && lives == 3) return true;
             return false;
         }
 
-
-
-
-
-        private static void RunUnitTest(string pattern_name, int repeat_count, List<string[]> testcase)
-        {
-            Console.Write($"測試案例: {pattern_name} ...");
-
-            try
-            {
-                bool[,] input_matrix = _Transform(testcase[0]);
-                for (int i = 1; i < testcase.Count * repeat_count; i++)
-                {
-                    bool[,] expected_matrix = _Transform(testcase[i % testcase.Count]);
-                    bool[,] actual_matrix = GetNextGenMatrix(input_matrix);
-
-                    // compare
-                    CompareMatrix(expected_matrix, actual_matrix);
-                    input_matrix = actual_matrix;
-                }
-            }
-            catch
-            {
-                Console.WriteLine(" FAILED.");
-                return;
-            }
-
-            Console.WriteLine(" PASSED.");
-        }
-
-        private static void CompareMatrix(bool[,] source, bool[,] target)
-        {
-            if (source == null) throw new ArgumentNullException();
-            if (target == null) throw new ArgumentNullException();
-            if (source.GetLength(0) != target.GetLength(0)) throw new ArgumentOutOfRangeException();
-            if (source.GetLength(1) != target.GetLength(1)) throw new ArgumentOutOfRangeException();
-
-            for (int y = 0; y < source.GetLength(1); y++)
-            {
-                for (int x = 0; x < source.GetLength(0); x++)
-                {
-                    if (source[x, y] != target[x, y]) throw new ArgumentException();
-                }
-            }
-
-            return;
-        }
-
-        private static bool[,] _Transform(string[] map)
-        {
-            bool[,] matrix = new bool[map[0].Length, map.Length];
-
-            int x = 0;
-            int y = 0;
-            foreach(var line in map)
-            {
-                foreach(var c in line)
-                {
-                    matrix[x, y] = (c == '1');
-                    x++;
-                }
-                x = 0;
-                y++;
-            }
-
-            return matrix;
-        }
     }
 }
