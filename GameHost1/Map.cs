5365597316fccac7e9540c56d111c49c3217b5ab
Fion <t4730@yahoo.com.tw>
2020-10-25 11:50:53 +0800

    Added Cell  and Map.

---
+++ GameHost1/Map.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameHost1
+{
+    public class Map
+    {
+        public bool[,] Matrix { get; set; }
+
+        public void Init(int w, int h, int rate = 20)
+        {
+            Matrix = new bool[w, h];
+            Random rnd = new Random();
+            for (int y = 0; y < Matrix.GetLength(1); y++)
+            {
+                for (int x = 0; x < Matrix.GetLength(0); x++)
+                {
+                    Matrix[x, y] = (rnd.Next(100) < rate);
+                }
+            }
+        }
+    }
+}
